758a266bbe75cf1604d4d4a5a94ae4861ac835ba
DinoChiesa <dpchiesa@hotmail.com>
2015-01-30 14:59:01 -0800

    adding wsd-mode.el to the repo

---
+++ wsd-core.el
@@ -0,0 +1,186 @@
+
+;;; Code:
+
+;; user-customizable sections
+
+;; only required for premium-features.
+(defvar wsd-api-key "")
+
+;; "svg" is also a permitted format, but this requires a premium account
+;; and thus a api-key.
+(defvar wsd-format "png")
+(defvar wsd-style "modern-blue")
+
+;; actual code
+
+;; implementation based on documentation as found here:
+;; http://www.websequencediagrams.com/embedding.html
+
+(require 'url)
+(require 'json)
+
+(defconst wsd-base-url "http://www.websequencediagrams.com/")
+
+(defun wsd-get-apikey-section ()
+  "Returns a key-value pair for the API-key to be user in request data, delimiters included.
+   If no api-key is used, returns nil."
+  (if wsd-api-key
+      (concat "&apikey=" wsd-api-key)
+    ""))
+
+(defun wsd-encode (message)
+  "Encodes the provided message into something which can be transported over HTTP."
+  (let* ((encode1 (replace-regexp-in-string (regexp-quote "+")
+                                            (regexp-quote "%2B")
+                                            message))
+         (encode2 (url-encode-url encode1)))
+    encode2))
+
+(defun wsd-get-request-data (message)
+  "Gets the request-data for a HTTP post to the wsd.com API."
+  (let* ((encoded (wsd-encode message))
+         (apikey  (wsd-get-apikey-section)))
+    (concat "apiVersion=1"
+            "&format=" wsd-format
+            "&style=" wsd-style
+            "&message=" encoded
+            apikey)))
+
+(defun wsd-get-json (message)
+  "Sends the provided message to the server and returns the server's JSON-response."
+  (let* ((url-request-method        "POST")
+         (url-request-extra-headers '(("Content-Type" . "application/x-www-form-urlencoded")))
+         (url-request-data          (wsd-get-request-data message))
+         (wsd-response              (url-retrieve-synchronously wsd-base-url)))
+    (save-excursion
+      (switch-to-buffer wsd-response)
+
+      (goto-char (point-min))
+      ;; move to beginning of JSON response
+      (search-forward "{")
+      (backward-char)
+      ;; parse and return json at point
+
+      (let* ((json (json-read)))
+        (kill-buffer wsd-response)
+        json))))
+
+(defun wsd-get-image-url (json)
+  "Based on the server's JSON-response, extracts the image-url to the resulting image."
+  (let* ((url (concat wsd-base-url
+                      (cdr (assoc 'img json)))))
+    url))
+
+(defun wsd-get-errors (json)
+  "Based on the server's JSON-response, extracts error-elements."
+  (let* ((errors (cdr (assoc 'errors json))))
+    (append errors '())))
+
+(defun wsd-parse-error (entry)
+  "Parses a single error-response as returned by the WSD server."
+  (with-temp-buffer
+    (insert entry)
+    (goto-char (point-min))
+    (search-forward-regexp "Line \\([[:digit:]]+\\): \\(.*\\)")
+    (let* ((line-num          (string-to-number (match-string 1)))
+           (error-description (match-string 2)))
+      (cons line-num error-description))))
+
+(defun wsd-get-error-lines (error-list)
+  "Processes and parses all the errors in the provided list."
+  (mapcar 'wsd-parse-error error-list))
+
+(defun wsd-get-image-extension ()
+  "Returns the file-name extension to be used based on the current wsd-mode configuration."
+  (concat "." wsd-format))
+
+(defun wsd-get-temp-filename ()
+  "Returns an appropriate corresponding image-filename for a given non-persisted buffer."
+  (make-temp-file "wsd-" nil (wsd-get-image-extension)))
+
+(defun wsd-get-image-filename (name)
+  "Returns an appropriate corresponding image-filename for a given buffer."
+  (if name
+      (concat (file-name-sans-extension name) (wsd-get-image-extension))
+    nil))
+
+(defun wsd-get-image-buffer-name (buffer-name file-name)
+  "Returns an appropriate corresponding buffer name to display resulting image in."
+  (if (not buffer-name)
+      (concat "wsd-temp-buffer." wsd-format)
+    file-name))
+
+(defun wsd-display-image-inline (buffer-name file-name)
+  "Displays the provided image in the provided buffer. Buffer is created if non-existant."
+  (save-excursion
+    (switch-to-buffer buffer-name)
+    (iimage-mode t)
+
+    (read-only-mode -1)
+    (kill-region (point-min) (point-max))
+    ;; unless we clear the cache, the same cached image will
+    ;; always get redisplayed.
+    (clear-image-cache nil)
+    (insert-image (create-image file-name))
+    (read-only-mode t)))
+
+(defun wsd-image-format-supported-p ()
+  "Helper function to determine if we can display the image we're generating."
+  (image-type-available-p (intern wsd-format)))
+
+;; buffer-local state variables
+(defvar wsd-errors nil) ; for flycheck
+(defvar wsd-last-temp-file nil)
+
+(defun wsd-show-diagram-inline ()
+  "Attempts to show the diagram provided by the current buffer inside an Emacs-buffer.
+   If emacs lacks format for the given graphics-format it will be delegated to the
+   operating-system to open the local copy."
+  (interactive)
+  (let* ((orig-buffer (buffer-name))
+         (buffer-name (buffer-file-name))
+         (temp-name   (wsd-get-temp-filename))
+         ;; only required for saved buffers.
+         (file-name   (wsd-get-image-filename buffer-name))
+         (message (buffer-substring-no-properties (point-min) (point-max)))
+         (json    (wsd-get-json message))
+         (url     (wsd-get-image-url json))
+         (errors  (wsd-get-error-lines (wsd-get-errors json))))
+    (save-excursion
+      (set (make-local-variable 'wsd-errors) errors)
+      (url-copy-file url temp-name t)
+
+      ;; only copy to file when in a saved buffer
+      (when file-name
+        (copy-file temp-name file-name t t t)))
+
+    (if (display-graphic-p)
+        (if (wsd-image-format-supported-p)
+            (let* ((image-buffer-name (wsd-get-image-buffer-name buffer-name file-name))
+                   (buffer-exists     (get-buffer image-buffer-name)))
+              ;; display image from temp-area because of bug in OSX Emacs.
+              ;; https://github.com/josteink/wsd-mode/issues/11
+              (wsd-display-image-inline image-buffer-name temp-name)
+              (switch-to-buffer orig-buffer)
+              (when (not buffer-exists)
+                (switch-to-buffer-other-window image-buffer-name))
+
+              ;; avoid ending up with a flurry of temp-files.
+              (when wsd-last-temp-file
+                (delete-file wsd-last-temp-file))
+
+              (set (make-local-variable 'wsd-last-temp-file) temp-name))
+          (browse-url temp-name))
+      (message url))))
+
+
+(defun wsd-show-diagram-online ()
+  "Shows the current buffer on www.websequencediagrams.com"
+  (interactive)
+  (let* ((message      (buffer-substring-no-properties (point-min) (point-max)))
+         (encoded      (wsd-encode message))
+         (url          (concat wsd-base-url "?m=" encoded)))
+    (browse-url url)))
+
+(provide 'wsd-core)
+;;; wsd-core.el ends here
---
+++ wsd-mode.el
@@ -0,0 +1,183 @@
+;;; wsd-mode.el --- Emacs major-mode for www.websequencediagrams.com
+
+;; Author     : Jostein Kjonigsen <jostein@gmail.com>
+;; Created    : December 2014
+;; Modified   : December 2014
+;; Version    : 0.2.0
+;; Keywords   : wsd diagrams design process modelling uml
+;; X-URL      : https://github.com/josteink/wsd-mode
+;;
+
+;;; Commentary:
+;;
+;; This is a major-mode for modelling and editing sequence-diagrams
+;; using the syntax employed by the online service
+;; www.websequencediagrams.com.
+;;
+;; The mode supports inline rendering the diagrams through the API
+;; provided through the website and persisting these to image-files next
+;; to the files used to generate them.
+;;
+;; It will automatically activate for files with a WSD-extension.
+;;
+;;
+;; Features:
+;;
+;; - syntax higlighting of reccognized keywords
+;; - automatic indentation of block-statements
+;; - generating and saving diagrams generated through WSD's online API.
+;; - support for WSD premium features (svg-export, etc) if API-key is
+;;   provided.
+;; - rendering diagrams inline in emacs, or in external OS viewer if image
+;;   format is not supported by Emacs.
+;;
+;;
+;; Customization:
+;;
+;; The mode can be slightly customized. Documenting this fully is on the
+;; TODO-list.
+;;
+;; To create mode-specific emacs-customizations, please use the
+;; wsd-mode-hook.
+;;
+;; A short summary of customizable variables:
+;;
+;; - wsd-api-key (default blank. required for premium-features.)
+;; - wsd-format (default png. svg requires premium, thus api-key.)
+;; - wsd-style (default modern-blue)
+;; - wsd-indent-offset (default 4)
+;;
+
+;;; Versions:
+;;
+;;    0.1.0 - Initial release.
+;;    0.2.0 - Fix file-format bug and temp-buffer reuse.
+;;            Improved syntax-highlighting.
+;;            Fix OSX rendering issues.
+
+;;; Code:
+
+(require 'wsd-core)
+
+;; notes about derived mode here: http://www.emacswiki.org/emacs/DerivedMode
+
+(defun wsd-create-font-lock-list (face list)
+  (mapcar (lambda (i)
+            (cons i face))
+          list))
+
+(defun wsd-add-keywords (face list)
+  (let* ((klist (wsd-create-font-lock-list face list)))
+    (font-lock-add-keywords nil klist)))
+
+(defvar wsd-indent-offset 4
+  "*Indentation offset for `wsd-mode'.")
+
+(defun wsd-any (predicate list)
+  (let* ((result nil))
+    (dolist (item list)
+      (setq result (or result
+                       (funcall predicate item))))
+    result))
+
+(defun wsd-line-starts-with (keywords)
+  (beginning-of-line-text)
+  (let* ((word       (thing-at-point 'word t)))
+    (if (equal nil word)
+        nil
+      (wsd-any `(lambda (x) (equal ,(downcase word) x)) keywords))))
+
+(defun wsd-indent-line ()
+  "Indent current line for `wsd-mode'."
+  (interactive)
+  (indent-line-to (wsd-get-line-indent)))
+
+(defun wsd-get-string-at-point ()
+  (let* ((thing (thing-at-point 'word t)))
+    (if (equal nil thing)
+        nil
+      (downcase thing))))
+
+;; else not included as it doesnt affect the -overall- indentation either way
+(defconst wsd-indentation-keywords '("alt" "opt" "loop" "end"))
+
+(defun wsd-is-indentation-keyword (word)
+  (and (not (equal nil word))
+       (member word wsd-indentation-keywords)))
+
+(defun wsd-get-buffer-indentation-keywords ()
+  "Returns the list of indentation-keywords found from the current point in the buffer, back to the start."
+  (interactive)
+
+  (save-excursion
+    (beginning-of-line)
+
+    (let* ((words '()))
+      (while (not (equal (point) (point-min)))
+        (beginning-of-line-text)
+        (let* ((word (wsd-get-string-at-point)))
+          (when (wsd-is-indentation-keyword word)
+            (setq words (cons word words))))
+        (beginning-of-line)
+        (forward-line -1))
+      words)))
+
+(defun wsd-get-indentation-from-keywords (keywords)
+  "Get the overall indentation from the supplied keywords."
+  (let* ((indent-col   0)
+         (indent-plus  '("alt" "opt" "loop"))
+         (indent-minus '("end")))
+    (dolist (keyword keywords)
+      (when (member keyword indent-plus)
+        (setq indent-col (+ indent-col wsd-indent-offset)))
+      (when (member keyword indent-minus)
+        (setq indent-col (- indent-col wsd-indent-offset)))
+      (setq indent-col (max 0 indent-col)))
+    indent-col))
+
+(defun wsd-get-adjustment-indent ()
+  "Adjust overall document indentation with specific reverse compensation for branch-starting keywords based on the current line."
+  (if (wsd-line-starts-with '("alt" "opt" "else"))
+      (- 0 wsd-indent-offset)
+    0))
+
+(defun wsd-get-line-indent ()
+  (let* ((keywords          (wsd-get-buffer-indentation-keywords))
+         (keyword-indent    (wsd-get-indentation-from-keywords keywords))
+         (adjustment-indent (wsd-get-adjustment-indent)))
+    (+ keyword-indent adjustment-indent)))
+
+;;;###autoload
+(define-derived-mode wsd-mode fundamental-mode "wsd-mode"
+  "Major-mode for websequencediagrams.com"
+  (let* ((line-starters (mapcar
+                         (lambda (kw) (concat "^[[:space:]]*" kw))
+                         '("title" "participant" "deactivate" "activate"
+                           "alt" "else" "opt" "loop" "end"
+                           "note")))
+         (non-starters   '(" over " " right " " left " " of "
+                           " as "))
+         (all-keywords    (append non-starters line-starters)))
+    (wsd-add-keywords 'font-lock-keyword-face all-keywords))
+
+  ;; lines starting with # are actual commens
+  (let* ((operator-list '("-->-" "-->" "->+" "->-" "->" ": " "#"))
+         (operators      (wsd-create-font-lock-list 'font-lock-comment-face operator-list)))
+    ;;(setq operators (cons (cons (regexp-quote "#") 'font-lock-command-face) operators))
+    (font-lock-add-keywords nil operators))
+
+  (make-local-variable 'wsd-indent-offset)
+  (set (make-local-variable 'indent-line-function) 'wsd-indent-line))
+
+(define-key wsd-mode-map (kbd "C-c C-c") 'wsd-show-diagram-inline)
+(define-key wsd-mode-map (kbd "C-c C-e") 'wsd-show-diagram-online)
+(define-key wsd-mode-map (kbd "C-c C-k") 'wsd-strip-errors)
+
+
+;;; Autoload mode trigger
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.wsd$" . wsd-mode))
+
+(provide 'wsd-mode)
+
+;;; wsd-mode.el ends here
