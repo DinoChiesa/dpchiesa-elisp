aa910dcbc4663793c334bdbadff3c938bf6064d5
Dino Chiesa <dpchiesa@hotmail.com>
2018-10-19 11:12:41 -0700

    add jshint notes to defaultcontent for .js files

--- defaultcontent/Template.js
+++ defaultcontent/Template.js
@@ -2,7 +2,10 @@
 // ------------------------------------------------------------------
 //
 // created: @DATE@
-// last saved: <2018-January-02 15:58:50>
+// last saved: <2018-October-15 17:08:36>
+
+/* jshint esversion: 6, node: true */
+/* global process, console, Buffer */
 
 'use strict';
 
