00abd90a40aa0ad6285ee2e98a70553722d2cd40
Dino Chiesa <dpchiesa@hotmail.com>
2019-02-20 09:47:20 -0800

    change default esversion to 8

--- defaultcontent/Template.js
+++ defaultcontent/Template.js
@@ -2,9 +2,9 @@
 // ------------------------------------------------------------------
 //
 // created: @DATE@
-// last saved: <2018-October-15 17:08:36>
+// last saved: <2019-February-07 18:33:15>
 
-/* jshint esversion: 6, node: true */
+/* jshint esversion: 8, node: true */
 /* global process, console, Buffer */
 
 'use strict';
